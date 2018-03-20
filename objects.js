4b17923faa0f14b89267de4dbee1d42fb1ec9b8d
corytveten <corytveten@gmail.com>
2018-03-20 10:59:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,10 @@ var meals = {};
 var meals = new Object();
 
 var meals = {breakfast: "oatmeal"};
+
+var playlist = {kenny loggins: "Heart to Heart"};
+
+function updatePlaylist(obj, key, value) {
+  obj[key] = value
+  return obj
+}
