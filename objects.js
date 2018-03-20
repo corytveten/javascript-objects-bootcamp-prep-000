4313c6ffec2285232fefcd7a6e3ebfdfd9a7fac9
corytveten <corytveten@gmail.com>
2018-03-20 11:06:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,3 +10,8 @@ function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song
   return playlist
 }
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
